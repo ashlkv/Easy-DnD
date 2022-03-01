28298b282c12eccf8bfca991ed3027ab3804bc17
Anna Shishlyakova <anna.shishlyakova@gmail.com>
2022-03-01 22:14:41 +0100

    Simpler style copy when cloning an element

--- lib/src/ts/createDragImage.ts
+++ lib/src/ts/createDragImage.ts
@@ -38,21 +38,7 @@ function deepClone(el: HTMLElement): HTMLElement {
 function copyStyle(src: HTMLElement, destination: HTMLElement) {
     const computedStyle = window.getComputedStyle(src);
     for (const key of computedStyle) {
-        if (key === 'width') {
-            // IE11
-            let width = computedStyle.getPropertyValue("box-sizing") === 'border-box' ?
-                src.clientWidth :
-                src.clientWidth - parseFloat(computedStyle.paddingLeft) - parseFloat(computedStyle.paddingRight)
-            destination.style.setProperty("width", width + "px");
-        } else if (key === 'height') {
-            // IE11
-            let height = computedStyle.getPropertyValue("box-sizing") === 'border-box' ?
-                src.clientHeight :
-                src.clientHeight - parseFloat(computedStyle.paddingTop) - parseFloat(computedStyle.paddingBottom)
-            destination.style.setProperty("height", height + "px");
-        } else {
-            destination.style.setProperty(key, computedStyle.getPropertyValue(key), computedStyle.getPropertyPriority(key))
-        }
+        destination.style.setProperty(key, computedStyle.getPropertyValue(key), computedStyle.getPropertyPriority(key))
     }
     destination.style.pointerEvents = 'none';
 }
